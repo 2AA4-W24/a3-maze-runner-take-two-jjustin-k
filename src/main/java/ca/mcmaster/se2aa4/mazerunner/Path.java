19fed723236e3df7619a19e48ed20e94a107c926
jjustin-k <jjkwinecki@gmail.com>
2024-01-19 15:29:17 -0500

    Adding Path Class

---
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Path.java
@@ -0,0 +1,15 @@
+package ca.mcmaster.se2aa4.mazerunner;
+
+public class Path {
+
+    private final String path;
+    public Path(String user_path){
+        path = user_path;
+    }
+
+    public String getPath(){
+        return path;
+    }
+
+
+}
