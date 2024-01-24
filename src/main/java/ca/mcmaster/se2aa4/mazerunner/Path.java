b23872f66ba7c14081766cf5d55ae90823e1d7d4
jjustin-k <jjkwinecki@gmail.com>
2024-01-24 18:25:59 -0500

    Can now switch bettwen path forms

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Path.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Path.java
@@ -19,8 +19,77 @@ public class Path {
         path += String.valueOf(ch);
     }
 
-    public void changeForm(){
-        String expanded = "";
+    public void toCanonicalForm(){
+        StringBuilder expanded = new StringBuilder();
+        if(isFactored()){
+            StringBuilder number = new StringBuilder();
+            for(int i = 0; i < path.length(); i++){
+                if(Character.isDigit(path.charAt(i)) && path.charAt(i) != ' '){
+                   number.append(path.charAt(i));
+                }
+                else{
+                    if(!number.isEmpty()){
+                        for(int j = 0; j < Integer.parseInt(number.toString()); j++){
+                            if(path.charAt(i) == 'F'){
+                                expanded.append('F');
+                            }
+                            else if(path.charAt(i) == 'R'){
+                                expanded.append('R');
+                            }
+                            else if(path.charAt(i) == 'L'){
+                                expanded.append('L');
+                            }
+                        }
+                        number = new StringBuilder();
+                    }
+                    else{
+                        if(path.charAt(i) == 'F'){
+                            expanded.append('F');
+                        }
+                        else if(path.charAt(i) == 'R'){
+                            expanded.append('R');
+                        }
+                        else if(path.charAt(i) == 'L'){
+                            expanded.append('L');
+                        }
+                    }
+                }
+            }
+            path = expanded.toString();
+        }
+    }
+
+    public void toFactoredForm(){
+        StringBuilder factored = new StringBuilder();
+        if(!isFactored()){
+            char current = 'z';
+            int repeats = 0;
+            for(int i = 0; i < path.length(); i++){
+                if(i == 0){
+                    current= path.charAt(i);
+                }
+                else{
+                    if (path.charAt(i) != current) {
+                        if(repeats > 1){
+                            factored.append(repeats);
+                        }
+                        factored.append(current);
+                        repeats = 0;
+                        current = path.charAt(i);
+                    }
+
+                }
+                repeats ++;
+            }
+            if(repeats > 1){
+                factored.append(repeats);
+            }
+            factored.append(current);
+            path = factored.toString();
+        }
+    }
+
+    private boolean isFactored(){
         boolean has_num = false;
         for(int i = 0; i < path.length(); i++){
             if(Character.isDigit(path.charAt(i)) && path.charAt(i) != ' '){
@@ -28,44 +97,7 @@ public class Path {
                 break;
             }
         }
-        if(has_num){
-
-            String number = "";
-            for(int i = 0; i < path.length(); i++){
-                if(Character.isDigit(path.charAt(i)) && path.charAt(i) != ' '){
-                   number += path.charAt(i);
-                }
-                else{
-                    if(!number.isEmpty()){
-                        for(int j = 0; j < Integer.parseInt(number); j++){
-                            if(path.charAt(i) == 'F'){
-                                expanded += 'F';
-                            }
-                            else if(path.charAt(i) == 'R'){
-                                expanded += 'R';
-                            }
-                            else if(path.charAt(i) == 'L'){
-                                expanded += 'L';
-                            }
-                        }
-                        number = "";
-                    }
-                    else{
-                        if(path.charAt(i) == 'F'){
-                            expanded += 'F';
-                        }
-                        else if(path.charAt(i) == 'R'){
-                            expanded += 'R';
-                        }
-                        else if(path.charAt(i) == 'L'){
-                            expanded += 'L';
-                        }
-                    }
-                }
-            }
-            path = expanded;
-
-        }
+        return has_num;
     }
 
 
